4234f8a17b71909ec4e8bd5b464388569e936974
samthegitguy <55744004+samthegitguy@users.noreply.github.com>
2020-10-16 10:54:06 +1100

    Add first time wrapper

---
+++ installer/setup.bat
@@ -0,0 +1,10 @@
+echo Run this only if you are coding with Java for the first time.
+pause 
+cd ../..
+code webpack4j
+pause
+"Git-2.28.0-64-bit (1).exe"
+"JavaCodingPack-0.3.10 (1).exe"
+echo Thank you for installing! Maven is not required as there is already a maven wrapper. 
+echo However, you may need to select Yes when VSCode asks if you trust this wrapper.
+echo If you dont, well, just download maven.
